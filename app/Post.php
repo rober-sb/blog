f192b556447d53445f143ae93acb94c19eb2484c
Eliancito <eliansilva565@gmail.com>
2022-02-18 11:29:44 -0300

    Publicaciones y usuarios #1, fin

---
+++ app/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Sluggable;
+
+        /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
