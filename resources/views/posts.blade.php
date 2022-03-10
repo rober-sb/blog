c8ab79c3baabafed671564b87d5ee1a798fc8d9f
Eliancito <eliansilva565@gmail.com>
2022-03-10 03:22:23 -0300

    Fin

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,11 +9,6 @@
                 <div class="card-body">
                     @if ($post->image)
                         <img src="{{ $post->get_image }}" class="card-img-top">
-                        @if ($post->iframe)
-                            <div class="embed-responsive embed-responsive-16by9">
-                                {!! $post->iframe !!}
-                            </div>
-                        @endif
                     @elseif($post->iframe)
                             <div class="embed-responsive embed-responsive-16by9">
                                 {!! $post->iframe !!}
